f29e635f4853d74e04bff45682d8d2db8003be1b
Anjela47 <96664149+Anjela47@users.noreply.github.com>
2022-05-31 11:41:30 +0400

    Update script.js

--- rabbit_game/script.js
+++ rabbit_game/script.js
@@ -4,8 +4,8 @@ const rabbitCell = 1
 const homeCell = 2
 const wolfCell = 3
 const fenceCell = 4
-const wolfProcent = 0.65
-const fenceProcent = 0.45
+const wolfProcent = 0.6
+const fenceProcent = 0.4
  let character=[
     {
         name:"rabbit",
@@ -34,57 +34,58 @@ document.getElementById("startBtn").onclick = startGame
 function startGame(){
     array = createArray()
     setPositions(array)
+
 }
 
 function createArray() {
   const arraySize = parseInt(document.getElementById('selectNum').value)
   const array = new Array(arraySize).fill(emptyCell).map(() => new Array(arraySize).fill(emptyCell))
   
-  console.log(character)
   console.log(array)
   return array
 }
 
 function setPositions(array){
-    const wolfCount = Math.floor(array.length*wolfProcent)
-    const fenceCount = Math.floor(array.length*fenceProcent)
-    const isexit = []
-    setRabbitPosition(isexit,array.length)
-    setHomePosition(isexit,array.length)
+    const wolfCount = Math.ceil(array.length*wolfProcent)
+    const fenceCount = Math.ceil(array.length*fenceProcent)
+    setRabbitPosition(array)
+    setHomePosition(array)
     for(let i=0;i<wolfCount;i++){
-        setWolfPosition(isexit,array.length)
+        setWolfPosition(array)
     }
     for(let i=0;i<fenceCount;i++){
-        setFencePosition(isexit,array.length)
+        setFencePosition(array)
     }
 }
 
-function setRabbitPosition(isexit,arraySize){
-    setIndexes('rabbit',isexit,arraySize)
-    
+function setRabbitPosition(array){
+    setIndexes('rabbit',array)
 }
-function setHomePosition(isexit,arraySize){
-    setIndexes('home',isexit,arraySize)
+function setHomePosition(array){
+    setIndexes('home',array)
 }
-function setWolfPosition(isexit,arraySize){
-    setIndexes('wolf',isexit,arraySize)
+function setWolfPosition(array){
+    setIndexes('wolf',array)
 }
-function setFencePosition(isexit,arraySize){
-    setIndexes('fence',isexit,arraySize)
+function setFencePosition(array){
+    setIndexes('fence',array)
 }
 
-function setIndexes(characterNum,isexit,arraySize){
-    const i = randomIndexes(arraySize)
-    const j = randomIndexes(arraySize)
-    if(!isexit.includes(`${i}${j}`)){
-        isexit.push(`${i}${j}`)
-        const element = character.find( item => item.name === characterNum)
+function setIndexes(characterNum,array){
+    const j = randomIndexes(array)
+    const i = randomIndexes(array)
+   
+    if(array[i][j]===emptyCell){
+        console.log(i,j)
+        const element = character.find(item => item.name === characterNum)
         array[i][j] = element.num
-    }  
+    } else{
+        setIndexes(characterNum,array)
+    }
 }
 
-function  randomIndexes(arraySize){
-    const i = Math.floor(Math.random() * arraySize)
+function  randomIndexes(array){
+    const i = Math.floor(Math.random() * array.length)
     return i
 }
 
