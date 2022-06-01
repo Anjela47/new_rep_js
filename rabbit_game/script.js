381cd62a81d132a59c0a12c60e94d62fac7285a8
Anjela47 <96664149+Anjela47@users.noreply.github.com>
2022-06-01 10:35:49 +0400

    commit

--- rabbit_game/script.js
+++ rabbit_game/script.js
@@ -34,6 +34,7 @@ document.getElementById("startBtn").onclick = startGame
 function startGame(){
     array = createArray()
     setPositions(array)
+    rabbitEventMove(array)
 
 }
 
@@ -76,13 +77,90 @@ function setIndexes(characterNum,array){
     const i = Math.floor(Math.random() * array.length)
    
     if(array[i][j]===emptyCell){
-        console.log(i,j)
         const element = character.find(item => item.name === characterNum)
         array[i][j] = element.num
-    } else{
+    }else{
         setIndexes(characterNum,array)
     }
 }
 
+function rabbitEventMove(array){
+    document.addEventListener("keydown", function(event){
+        rabbitStep(array,event.key)
+    })
+}
+
+function rabbitStep(array,step){
+    const listOfIndexes = getCurrentDir(array,rabbitCell)
+    const i = listOfIndexes[0][0]
+    const j = listOfIndexes[0][1]
+    if(step === "ArrowLeft"){
+        goToLeft(array,i,j)
+    }else
+    if(step === "ArrowUp"){
+        goToUp(array,i,j)
+    }else
+    if(step === "ArrowRight"){
+        goToRight(array,i,j)
+    }else
+    if(step === "ArrowDown"){
+        goToDown(array,i,j)
+    }
+    
+    console.log(array)
+}
+function goToLeft(array,i,j){
+        if(j === 0 && array[i][array.length-1] === emptyCell){
+            array[i][array.length-1] = array[i][j]
+            array[i][j] = emptyCell
+        }else
+        if(array[i][j-1] === emptyCell){
+            array[i][j-1] = array[i][j]
+            array[i][j] = emptyCell
+        }
+    
+}
+function goToUp(array,i,j){
+    if(i === 0 && array[array.length-1][j] === emptyCell){
+        array[array.length-1][j] = array[i][j]
+        array[i][j] = emptyCell
+    }else
+    if(array[i-1][j] === emptyCell){
+        array[i-1][j] = array[i][j]
+        array[i][j] = emptyCell
+    }
+}
+function goToRight(array,i,j){
+    if(j === array.length-1 && array[i][0] === emptyCell){
+        array[i][0] = array[i][j]
+        array[i][j] = emptyCell
+    }else
+    if(array[i][j+1] === emptyCell){
+        array[i][j+1] = array[i][j]
+        array[i][j] = emptyCell
+    }
+}
+function goToDown(array,i,j){
+    if(i === array.length-1 && array[0][j] === emptyCell){
+        array[0][j] = array[i][j]
+        array[i][j] = emptyCell
+    }else
+    if(array[i+1][j] === emptyCell){
+        array[i+1][j] = array[i][j]
+        array[i][j] = emptyCell
+    }
+    
+}
 
 
+function getCurrentDir(array,character){
+    const getFromArray = function(acc, row, i){
+        row.forEach((item, j) => {
+            if(item === character){
+                acc.push([i, j])
+            }
+        })
+        return acc
+    }
+    return  array.reduce(getFromArray, [])
+}
