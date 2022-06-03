df215934d1f68f8735620296e46dbf8d3b1906d7
Anjela47 <96664149+Anjela47@users.noreply.github.com>
2022-06-03 09:36:33 +0400

    commit

--- rabbit_game/script.js
+++ rabbit_game/script.js
@@ -1,54 +1,55 @@
 let arr = []
-const emptyCell = 0
-const rabbitCell = 1
-const homeCell = 2
-const wolfCell = 3
-const fenceCell = 4
-const wolfProcent = 0.6
-const fenceProcent = 0.4
- let character=[
+const EMPTY_CELL = 0
+const RABBIT_CELL = 1
+const HOME_CELL = 2
+const WOLF_CELL = 3
+const FENCE_CELL = 4
+const WOLF_PROCENT = 0.6
+const FENCE_PROCENT = 0.4
+
+let character=[
     {
         name:"rabbit",
         img:"/images/rabbit.png",
-        num:rabbitCell 
+        num:RABBIT_CELL 
     },
     {
         name:"home",
         img:"/images/home.png",
-        num:homeCell
+        num:HOME_CELL
     },
     {
         name:"wolf",
         img:"/images/wolf.png",
-        num:wolfCell
+        num:WOLF_CELL
     },
     {
         name:"fence",
         img:"/images/fence.png",
-        num:fenceCell
+        num:FENCE_CELL
     }
-
 ]
+
 document.getElementById("startBtn").onclick = startGame
 
 function startGame(){
     array = createArray()
     setPositions(array)
+    DrawBoard(array)
     rabbitEventMove(array)
-
 }
 
-function createArray() {
+function createArray(){
   const arraySize = parseInt(document.getElementById('selectNum').value)
-  const array = new Array(arraySize).fill(emptyCell).map(() => new Array(arraySize).fill(emptyCell))
+  const array = new Array(arraySize).fill(EMPTY_CELL).map(() => new Array(arraySize).fill(EMPTY_CELL))
   
-  console.log(array)
+  
   return array
 }
 
 function setPositions(array){
-    const wolfCount = Math.ceil(array.length*wolfProcent)
-    const fenceCount = Math.ceil(array.length*fenceProcent)
+    const wolfCount = Math.ceil(array.length*WOLF_PROCENT)
+    const fenceCount = Math.ceil(array.length*FENCE_PROCENT)
     setRabbitPosition(array)
     setHomePosition(array)
     for(let i=0;i<wolfCount;i++){
@@ -76,7 +77,7 @@ function setIndexes(characterNum,array){
     const j = Math.floor(Math.random() * array.length)
     const i = Math.floor(Math.random() * array.length)
    
-    if(array[i][j]===emptyCell){
+    if(array[i][j]===EMPTY_CELL){
         const element = character.find(item => item.name === characterNum)
         array[i][j] = element.num
     }else{
@@ -87,50 +88,59 @@ function setIndexes(characterNum,array){
 function rabbitEventMove(array){
     document.addEventListener("keydown", function(event){
         rabbitStep(array,event.key)
-        wolfMove(array)
+        wolfStep(array)
+        DrawBoard(array)
     })
 }
 
-function wolfMove(array){
-    const listOfWolfIndexes = getCurrentDir(array,wolfCell)
-    const listOfRabbitIndex = getCurrentDir(array,rabbitCell)[0]
+function wolfStep(array){
+    const listOfWolfIndexes = getCurrentDir(array,WOLF_CELL)
+    const listOfRabbitIndex = getCurrentDir(array,RABBIT_CELL)[0]
     let requiredIndex
-    listOfWolfIndexes.forEach(element => {
-        const [wolf_i,wolf_j] = element
-        const up = [wolf_i-1,wolf_j] 
-        const right = [wolf_i,wolf_j+1] 
-        const down = [wolf_i+1,wolf_j] 
-        const left = [wolf_i,wolf_j-1]
-        const wolfAreaIndexes = []
-        if(checkValid(up,array)){
-            wolfAreaIndexes.push(up)
-        } 
-        if(checkValid(right,array)){
-            wolfAreaIndexes.push(right)
-        } 
-        if(checkValid(down,array)){
-            wolfAreaIndexes.push(down)
-        } 
-        if(checkValid(left,array)){
-            wolfAreaIndexes.push(left)
-        } 
-        requiredWolfAreaIndexes = wolfAreaIndexes.filter(item=> array[item[0]][item[1]]===0)
+    listOfWolfIndexes.forEach(index => {
+        const requiredWolfAreaIndexes = getRequiredWolfAreaIndexes(array, index)
         let min=50
-        
         requiredWolfAreaIndexes.forEach(element => {
-            
-            d = findNearestDistance(element,listOfRabbitIndex)
+            d = findDistances(element,listOfRabbitIndex)
             if(d<min){
                 min = d
                 requiredIndex = element
             }
         })
-        array[requiredIndex[0]][requiredIndex[1]] = wolfCell
-        array[wolf_i][wolf_j] = emptyCell
+        wolfMove(requiredIndex,index)
+       
     })    
    
 }
-function findNearestDistance(element,listOfRabbitIndex){
+function wolfMove(requiredIndex,index){
+   
+    array[requiredIndex[0]][requiredIndex[1]] = WOLF_CELL
+    array[index[0]][index[1]] = EMPTY_CELL
+}
+
+function getRequiredWolfAreaIndexes(array,index){
+    const [wolf_i,wolf_j] = index
+    const up = [wolf_i-1,wolf_j] 
+    const right = [wolf_i,wolf_j+1] 
+    const down = [wolf_i+1,wolf_j] 
+    const left = [wolf_i,wolf_j-1]
+    const wolfAreaIndexes = []
+    if(checkValid(up,array)){
+        wolfAreaIndexes.push(up)
+    } 
+    if(checkValid(right,array)){
+        wolfAreaIndexes.push(right)
+    } 
+    if(checkValid(down,array)){
+        wolfAreaIndexes.push(down)
+    } 
+    if(checkValid(left,array)){
+        wolfAreaIndexes.push(left)
+    } 
+    return  wolfAreaIndexes.filter(item=> array[item[0]][item[1]]===EMPTY_CELL)
+    
+}
+function findDistances(element,listOfRabbitIndex){
     d = Math.sqrt(Math.pow((listOfRabbitIndex[0]-element[0]),2) + Math.pow((listOfRabbitIndex[1]-element[1]),2))
     return d
 }
@@ -140,90 +150,60 @@ function checkValid(index,array){
      }
 }
 function rabbitStep(array,step){
-    const listOfIndexes = getCurrentDir(array,rabbitCell)[0]
+    const listOfIndexes = getCurrentDir(array,RABBIT_CELL)[0]
     const [i,j] = listOfIndexes
-    
     if(step === "ArrowLeft"){
-        goToLeft(array,i,j)
+        const newcoord = [i,j-1]
+        moveRabbit(array,listOfIndexes,newcoord)
     }else
     if(step === "ArrowUp"){
-        goToUp(array,i,j)
+        moveRabbit(array,listOfIndexes,[i-1,j])
     }else
     if(step === "ArrowRight"){
-        goToRight(array,i,j)
+        moveRabbit(array,listOfIndexes,[i,j+1])
     }else
     if(step === "ArrowDown"){
-        goToDown(array,i,j)
+        moveRabbit(array,listOfIndexes,[i+1,j])
     }
-    
+    // DrawBoard(array)
     console.log(array)
-}
-function goToLeft(array,i,j){
-    if(array[i][j-1]===homeCell){
-        alert("you win")
-    }else
-    if(array[i][j-1]===homeCell){
-        alert("game over")
-    }
-    else if(j === 0 && array[i][array.length-1] === emptyCell){
-        array[i][array.length-1] = array[i][j]
-        array[i][j] = emptyCell
-    }else
-    if(array[i][j-1] === emptyCell){
-        array[i][j-1] = array[i][j]
-        array[i][j] = emptyCell
-    }
-        
-}
-function goToUp(array,i,j){
-    if(array[i-1][j]===homeCell){
-        alert("you win")
-    }else if(array[i-1][j]===homeCell){
-        alert("game over")
-    }else
-    if(i === 0 && array[array.length-1][j] === emptyCell){
-        array[array.length-1][j] = array[i][j]
-        array[i][j] = emptyCell
-    }else
-    if(array[i-1][j] === emptyCell){
-        array[i-1][j] = array[i][j]
-        array[i][j] = emptyCell
-    }
-}
-function goToRight(array,i,j){
-    if(array[i][j+1]===homeCell){
-        alert("you win")
-    }else
-    if(array[i][j+1]===homeCell){
-        alert("game over")
-    }else
-    if(j === array.length-1 && array[i][0] === emptyCell){
-        array[i][0] = array[i][j]
-        array[i][j] = emptyCell
-    }else
-    if(array[i][j+1] === emptyCell){
-        array[i][j+1] = array[i][j]
-        array[i][j] = emptyCell
-    }
-}
-function goToDown(array,i,j){
-    if(array[i+1][j]===homeCell){
-        alert("you win")
-    }else
-    if(array[i+1][j]===homeCell){
-        alert("game over")
-    }else
-    if(i === array.length-1 && array[0][j] === emptyCell){
-        array[0][j] = array[i][j]
-        array[i][j] = emptyCell
-    }else
-    if(array[i+1][j] === emptyCell){
-        array[i+1][j] = array[i][j]
-        array[i][j] = emptyCell
-    }
     
 }
 
+function moveRabbit(array,old,newcoord){
+    if(newcoord[0]===-1 && array[array.length-1][old[1]]!=FENCE_CELL){
+        winOrLose(array[array.length-1][old[1]])
+        array[array.length-1][old[1]] = RABBIT_CELL
+        
+    }else
+    if(newcoord[1]===-1 && array[old[0]][array.length-1]!=FENCE_CELL){
+        winOrLose(array[old[0]][array.length-1])
+        array[old[0]][array.length-1] = RABBIT_CELL
+    }else
+    if(newcoord[0]===array.length && array[0][old[1]]!=FENCE_CELL){
+        winOrLose(array[0][old[1]])
+        array[0][old[1]] = RABBIT_CELL
+    }else
+    if(newcoord[1]===array.length && array[old[0]][0]!=FENCE_CELL){
+        winOrLose( array[old[0]][0])
+        array[old[0]][0] = RABBIT_CELL
+    }else
+    if(array[newcoord[0]][newcoord[1]] != FENCE_CELL){
+        winOrLose(array[newcoord[0]][newcoord[1]])
+        array[newcoord[0]][newcoord[1]] = RABBIT_CELL
+    }
+    array[old[0]][old[1]] = EMPTY_CELL
+    
+
+}
+function winOrLose(location){
+    if(location===HOME_CELL){
+        alert("You win")
+    }else
+    if(location==WOLF_CELL){
+        alert("GAME OVER")
+    }
+}
 
 function getCurrentDir(array,character){
     const getFromArray = function(acc, row, i){
@@ -235,4 +215,49 @@ function getCurrentDir(array,character){
         return acc
     }
     return  array.reduce(getFromArray, [])
+}
+
+
+function DrawBoard(array){
+    board = document.getElementById('board')
+    board.innerHTML = ""
+    const width = array.length*60 + 2*array.length
+    board.style.width = `${width}px`
+    board.style.height = `${width}px`
+    for(let i=0; i<array.length;i++){
+        for(let j=0; j<array.length;j++){
+        
+       div = generateDiv(array,i,j)
+       
+        board.append(div)
+        
+        }
+}
+}
+
+function generateDiv(array,i,j){
+    const div = document.createElement("div")
+    div.id=`${i}${j}`
+    div.className="box"
+    img = generateImg(array[i][j])
+    div.appendChild(img)
+    return div
+}
+function generateImg(coord){
+    img = document.createElement("img")
+    if(coord===RABBIT_CELL){
+        img.src = "images/rabbit.png"
+        
+    }
+    if(coord===FENCE_CELL){
+        img.src = "images/fence.png"
+    }
+    if(coord===HOME_CELL){
+        img.src = "images/home.jfif"
+    }
+    if(coord===WOLF_CELL){
+        img.src = "images/wolf.png"
+    }
+    return img
+    
 }
